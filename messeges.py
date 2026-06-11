ecdf9b11755db6d8bfae3e7ff22d094e1c089ffb
No1se-pi <skillday450@gmail.com>
2026-06-11 14:40:02 +0300

    Language, bug fixes, logic improvements

---
+++ messeges.py
@@ -0,0 +1,27 @@
+from dotenv import find_dotenv
+
+lang_custodes = {
+"eng": f"""
+Commit blocked by Custodes.
+Stop words detected in the last commit index.
+You can find the full list of words in the .env file {find_dotenv()}
+""",
+
+"ru": f"""
+Коммит заблокирован Наблюдателем.
+Стоп слова обнаружены в последнем индексе коммита.
+Вы можете ознакомится с полным списком слов в файле .env {find_dotenv()}
+"""
+}
+
+logo_custodes = """
+   |\                 /|
+   | \               / |
+   |愛\     /\      /静|
+   |  /    /  \     \  |
+   | /    / /\ \     \ |
+   |/    / /()\ \     \|
+   |\   / /____\ \    /|
+   | \ /________\ \  / |
+   |  /___________ \   |
+"""
--- parser.py
+++ parser.py
@@ -1,10 +1,18 @@
 import subprocess
 import os
 from dotenv import load_dotenv
+import sys
 
-load_dotenv()
-banwords = os.getenv("banwords").split(",")
+from messeges import *
 
+#______variables and settings______
+load_dotenv() #loading env
+banwords = os.getenv("banwords").split(",") #list of all banned words
+strict_compliance = lang_custodes[os.getenv("lang_custodes", "eng")]
+
+blocked_lines = [] #list of all "dangerous" lines
+
+#______the basic logic______
 process = subprocess.Popen(
     ["git","diff","--staged"],
     stdout=subprocess.PIPE,
@@ -13,11 +21,15 @@ process = subprocess.Popen(
 
 for line in process.stdout:
     for ban_word in banwords:
-        if ban_word in line and line[0] == "+":
-            print(line)
-            print(
-                '''
-ALLERT SUKA BOY!!!
-DONT PUSH MAFAKA BITCH
-                '''
-            )
+
+        if ban_word in line and line[0] == "+" and line[0:3] != "+++":
+            blocked_lines.append(line)
+
+if len(blocked_lines) > 0:
+    if os.getenv("logo_custodes") == "yes":
+        print(logo_custodes)
+    print(strict_compliance)
+    print(*blocked_lines, sep="\n")
+    sys.exit(1)
+
+sys.exit(0)
